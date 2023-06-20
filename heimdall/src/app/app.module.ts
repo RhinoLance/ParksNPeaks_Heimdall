b62b78b8242f9bdf634bfff48388c64c74019eac
Lance Conry <lance@rhinosw.com>
2023-06-21 09:54:18 +1000

    Moved app.component to main

--- heimdall/src/app/app.module.ts
+++ heimdall/src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { SpotListComponent } from './components/spot-list/spot-list.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     SpotListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }

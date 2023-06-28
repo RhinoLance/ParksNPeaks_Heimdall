63648b15736d4b7d78fdd5dc49cbf6e4a69317c8
Lance Conry <lance@rhinosw.com>
2023-06-28 12:27:59 +1000

    Removed stock test which failed.

--- heimdall/src/app/components/main/main.component.spec.ts
+++ heimdall/src/app/components/main/main.component.spec.ts
@@ -21,13 +21,4 @@ describe("AppComponent", () => {
 		const app = fixture.componentInstance;
 		expect(app.title).toEqual("heimdall");
 	});
-
-	it("should render title", () => {
-		const fixture = TestBed.createComponent(MainComponent);
-		fixture.detectChanges();
-		const compiled = fixture.nativeElement as HTMLElement;
-		expect(compiled.querySelector(".content span")?.textContent).toContain(
-			"heimdall app is running!"
-		);
-	});
 });

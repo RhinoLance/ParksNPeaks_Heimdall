f65b53961cd0988e644dc3853599a68f2413d1f7
Lance Conry <lance@rhinosw.com>
2023-09-05 20:25:39 +1000

    Added timeAgo and redirect

--- heimdall/src/app/components/activation/activation.component.ts
+++ heimdall/src/app/components/activation/activation.component.ts
@@ -4,13 +4,21 @@ import { Spot } from "src/app/models/Spot";
 import { SpotHistoryCardComponent } from "../spot-history-card/spot-history-card.component";
 import { ModeBadgeComponent } from "../../mode-badge/mode-badge.component";
 import { NgIf, NgFor, NgClass } from "@angular/common";
+import { NgPipesModule } from "ngx-pipes";
 
 @Component({
 	selector: "pph-activation",
 	templateUrl: "./activation.component.html",
 	styleUrls: ["./activation.component.scss"],
 	standalone: true,
-	imports: [NgIf, NgFor, NgClass, ModeBadgeComponent, SpotHistoryCardComponent],
+	imports: [
+		NgIf,
+		NgFor,
+		NgClass,
+		ModeBadgeComponent,
+		SpotHistoryCardComponent,
+		NgPipesModule,
+	],
 })
 export class ActivationComponent implements OnInit {
 	@Input() public activation!: Activation;
--- heimdall/src/app/components/spot-history-card/spot-history-card.component.ts
+++ heimdall/src/app/components/spot-history-card/spot-history-card.component.ts
@@ -2,13 +2,14 @@ import { Component, Input, OnInit } from "@angular/core";
 import { Spot } from "src/app/models/Spot";
 import { ModeBadgeComponent } from "../../mode-badge/mode-badge.component";
 import { NgIf, NgClass, NgStyle, NgFor } from "@angular/common";
+import { NgPipesModule } from "ngx-pipes";
 
 @Component({
 	selector: "pph-spot-history-card",
 	templateUrl: "./spot-history-card.component.html",
 	styleUrls: ["./spot-history-card.component.scss"],
 	standalone: true,
-	imports: [NgIf, NgClass, NgStyle, NgFor, ModeBadgeComponent],
+	imports: [NgIf, NgClass, NgStyle, NgFor, ModeBadgeComponent, NgPipesModule],
 })
 export class SpotHistoryCardComponent implements OnInit {
 	@Input() public spotList: Spot[] = [];
@@ -19,7 +20,7 @@ export class SpotHistoryCardComponent implements OnInit {
 	};
 
 	public ngOnInit(): void {
-		this.viewState.bodyDisplay = "expanded";
+		this.viewState.bodyDisplay = this.expanded ? "expanded" : "collapsed";
 	}
 
 	public toggleBodyDisplay(): void {

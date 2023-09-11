0a7314d9946bca2f12416389b68f2f2cc996bd8b
Lance Conry <lance@rhinosw.com>
2023-09-11 12:48:48 +1000

    Added elapsed time state indicator

--- heimdall/src/app/components/activation/activation.component.ts
+++ heimdall/src/app/components/activation/activation.component.ts
@@ -5,6 +5,7 @@ import { SpotHistoryCardComponent } from "../spot-history-card/spot-history-card
 import { ModeBadgeComponent } from "../../mode-badge/mode-badge.component";
 import { NgIf, NgFor, NgClass } from "@angular/common";
 import { TimeagoModule } from "ngx-timeago";
+import { timer } from "rxjs";
 
 @Component({
 	selector: "pph-activation",
@@ -26,6 +27,7 @@ export class ActivationComponent implements OnInit {
 	public viewState: ViewState = {
 		spot: new Spot(),
 		supersededSpotList: [],
+		elapsedTimeState: ElapsedTimeState.Active,
 	};
 
 	public readonly liveTimeAgo: boolean = true;
@@ -40,10 +42,33 @@ export class ActivationComponent implements OnInit {
 					.reverse();
 			});
 		}
+
+		timer(0, 1000 * 60).subscribe((_) => {
+			this.setTimeElapsedState();
+		});
 	}
+
+	private setTimeElapsedState(): void {
+		const diffMinutes =
+			(new Date().getTime() - this.viewState.spot.time.getTime()) / 1000 / 60;
+
+		this.viewState.elapsedTimeState =
+			diffMinutes < 15
+				? ElapsedTimeState.Active
+				: diffMinutes < 60
+				? ElapsedTimeState.Shoulder
+				: ElapsedTimeState.Inactive;
+	}
+}
+
+export enum ElapsedTimeState {
+	Active = "active",
+	Shoulder = "shoulder",
+	Inactive = "inactive",
 }
 
 interface ViewState {
 	spot: Spot;
 	supersededSpotList: Spot[];
+	elapsedTimeState: ElapsedTimeState;
 }

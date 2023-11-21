c52b34863c7f39f5fbb94d030f1b5af99702b803
Lance Conry <lance@rhinosw.com>
2023-11-21 15:15:10 +1100

    Fixed tests for connectionStatusComponent

--- heimdall/src/app/components/connection-status/connection-status.component.spec.ts
+++ heimdall/src/app/components/connection-status/connection-status.component.spec.ts
@@ -1,14 +1,26 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ConnectionStatusComponent } from "./connection-status.component";
+import { RealTimeUserService } from "src/app/services/RealTimeUserService";
+import { RealTimeUserServiceMock } from "src/app/services/RealTimeUserServiceMock";
+import { BehaviorSubject, Subject } from "rxjs";
+import { HubUser } from "src/app/services/HeimdallSignalRService";
 
 describe("ConnectionStatusComponent", () => {
 	let component: ConnectionStatusComponent;
 	let fixture: ComponentFixture<ConnectionStatusComponent>;
 
+	const rtsMock = {
+		currentClientCount: new BehaviorSubject<number>(1),
+		connectionStateChanged: new BehaviorSubject<boolean>(true),
+	};
+
 	beforeEach(() => {
 		TestBed.configureTestingModule({
 			imports: [ConnectionStatusComponent],
+			providers: [
+				{ provide: RealTimeUserService, useValue: rtsMock },
+			]
 		});
 		fixture = TestBed.createComponent(ConnectionStatusComponent);
 		component = fixture.componentInstance;
